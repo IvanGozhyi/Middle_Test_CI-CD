9bdf5dfd832543d98eac6d2b7d5f4e48811f0cf9
Ivan <givan2981@gmail.com>
2026-04-10 14:06:56 +0300

    def process added

--- processor.py
+++ processor.py
@@ -6,3 +6,18 @@ class FileProcessor:
         self.input_path = input_path
         self.output_path = output_path
         self.matcher = matcher
+
+    def process(self) -> tuple[int, int]:
+        total_lines = 0
+        lines_found = 0
+
+        with self.input_path.open('r', encoding='utf-8') as infile, \
+                self.output_path.open('w', encoding='utf-8') as outfile:
+
+            for line in infile:
+                total_lines += 1
+                if self.matcher.match(line):
+                    outfile.write(line)
+                    lines_found += 1
+
+        return total_lines, lines_found
